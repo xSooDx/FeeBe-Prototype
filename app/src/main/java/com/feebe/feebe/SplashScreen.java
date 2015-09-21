1ece0c45b5cd5ffbbdbcc4fae9f39167e78dfa87
xSooDx <soodsaurabh95@gmail.com>
2015-09-21 21:02:13 +0530

    Feel Better App.
    Splash Screen and Login Page.

---
+++ app/src/main/java/com/feebe/feebe/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.feebe.feebe;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by SooD on 20-09-2015.
+ */
+public class SplashScreen extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    //TODO: attempt network connection
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
